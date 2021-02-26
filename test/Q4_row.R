d198faa63022645fb80924f9890fd350ad06963b
Mengyu5 <mhe20@jhu.edu>
2021-02-26 16:14:39 -0500

    edit

--- test/Q4_row.R
+++ test/Q4_row.R
@@ -5,7 +5,4 @@ file = readLines(list.files(pattern = "\\.Rmd|\\.rmd")[1])
 
 stopifnot(!is.null(grep("rowMeans", file, value = TRUE)))
 
-stopifnot(!is.null(grep("colMeans", file, value = TRUE)))
-
-
 
