4b765d2e11865170798f7447d599b40dd44ac26e
Mengyu5 <56314224+Mengyu5@users.noreply.github.com>
2021-02-27 11:08:26 -0500

    Update Q6_code.R

--- test/Q6_code.R
+++ test/Q6_code.R
@@ -2,7 +2,7 @@
 
 file = readLines(list.files(pattern = "\\.Rmd|\\.rmd")[1])
 
-tmp = grep("geom_bar|geom_col|barplot", file)
+tmp = grep("geom_bar|geom_col|barplot|stat_count", file)
 
 stopifnot(tmp[length(tmp)] - tmp[1] > 10)
 
